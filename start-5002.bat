f0ff5b6bda3ef184ad28b6890e28ad3dc96d6e90
balachander1205 <digumarthi.bala@gmail.com>
2023-07-03 23:25:18 +0530

    updated fixes reported on 6/6/2023

--- start-5002.bat
+++
@@ -1,6 +0,0 @@
-title image validation api server
-rem venv/Scripts/activate -exit
-rem echo Starting image validation application
-waitress-serve --port 5002 --host 0.0.0.0 --call app:app
-rem python app.py
-pause
